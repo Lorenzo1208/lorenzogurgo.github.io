01e59e374d92a0b167b5ee2543dc3f7626321d22
Lorenzo G <55632146+Lolo8021@users.noreply.github.com>
2022-10-14 16:20:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Étudiant", "Développeur", "Micro-Entrepreneur", "Investisseur", "Freelancer"],
+        strings: ["Jeune", "Junior", "Débutant"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Étudiant", "Développeur", "Micro-Entrepreneur", "Investisseur", "Freelancer"],
+        strings: ["Jeune", "Junior", "Débutant"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
